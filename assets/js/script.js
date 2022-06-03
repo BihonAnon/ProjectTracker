c479aadbc43626be98f9886431f2cbfb66fb8a98
byron <byrondalberg@gmail.com>
2022-06-03 11:23:56 -0700

    HTML Bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var formEl = $('#project-form');
+var projectNameEl = $('input[name="project-name"]');
+var projectTypeEl = $('input[name="project-type"]');
+var hourlyWageEl = $('input[name="hourly-wage"]');
+var dueDateEl = $('input[name="due-date"]');
